0f67a6b56ac6c9110239675097dd03498ef2cd1a
Florent BELLENGER <bellengerflorent@gmail.com>
2020-07-03 10:57:23 +0200

    quest symphony 5

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -7,16 +7,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wild", name="wild")
+ */
 Class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/", name="wild_index")
      */
-    public function index() :Response
+    public function index()
     {
         return $this->render('Wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
     }
-
+    /**
+     * @Route("/show/{title}", name="wild_show", requirements={"title"="[a-z0-9/-]+"}, defaults={"title"=null})
+     */
+    public function show(?string $title)
+    {
+        if ($title === null){
+            $title ="Aucune série sélectionnée, veuillez choisir une série";
+        }
+        return $this->render("Wild/show.html.twig", [
+            "title"=>$title,
+        ]);
+    }
 }
